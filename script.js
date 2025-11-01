8f625a965b09914d9fa8fbbbcb85f049fcd6d4ad
Ludhill <38694381+ludhill@users.noreply.github.com>
2025-11-01 13:25:06 -0300

    Modify query to filter ID range from 280 to 300
    Updated the query to filter results between IDs 280 and 300. Pois não validaram a ideia

--- script.js
+++ script.js
@@ -24,7 +24,10 @@ const btnWhatsapp = document.getElementById('btn-whatsapp');
  
 let shoppingCart = []; 
 async function carregarGrelha() {
-    const { data, error } = await supabaseClient.from('numeros').select().order('id', { ascending: true });
+    //const { data, error } = await supabaseClient.from('numeros').select().order('id', { ascending: true });
+ // Nova Linha (com o filtro):
+    const { data, error } = await supabaseClient.from('numeros').select().gte('id', 280).lte('id', 300).order('id', { ascending: true });
+
     if (error) {
         console.error('Erro ao carregar números:', error);
         grelha.innerHTML = '<p>Erro ao carregar números. Tente novamente.</p>';
